686d09d6ec58e34fae7d0f247b520d037e438599
Saloni Kaur <salonikaurone@gmail.com>
2021-04-17 00:55:39 +0800

    mock http call

--- test/marvel/marvel.service.spec.ts
+++ test/marvel/marvel.service.spec.ts
@@ -1,3 +1,6 @@
+import { of } from 'rxjs';
+import { HttpService } from '@nestjs/common';
+import { AxiosResponse } from 'axios';
 import { Character } from './../../src/marvel/model/character.model';
 import { MarvelService } from './../../src/marvel/marvel.service';
 import { Test } from '@nestjs/testing';
@@ -7,6 +10,7 @@ jest.setTimeout(200000);
 
 describe('MarvelServiceTest', () => {
     let marvelService: MarvelService;
+    let mockHttpService: HttpService;
 
     beforeAll(async () => {
         const testingModule = await Test.createTestingModule({
@@ -17,12 +21,37 @@ describe('MarvelServiceTest', () => {
             providers: [MarvelService],
         }).compile();
         marvelService = testingModule.get<MarvelService>(MarvelService);
+        mockHttpService = testingModule.get<HttpService>(HttpService);
     });
 
-    it('calls multiple times', async () => {
-        process.env.PRIVATE_API_KEY = "gibberish";
-        process.env.PUBLIC_API_KEY = "gibberish";
+    it('returns response of api', async () => {
+
+        const mock: AxiosResponse = {
+            data: {data: {results: [{id: 123, name: "123"}], total: 1}},
+            status: 200,
+            statusText: 'OK',
+            headers: {},
+            config: {}
+          };
+        jest.spyOn(mockHttpService, 'get').mockImplementation(() => of(mock));
         const result: Character[] = await marvelService.getAllCharacters();
-        expect(result.length).toBeGreaterThan(100);
+        expect(result).toHaveLength(1);
+        expect(result[0].id).toEqual(123);
+    });
+
+    it('handles multiple result response of api', async () => {
+
+        const mock: AxiosResponse = {
+            data: {data: {results: [{id: 123, name: "123"}, {id: 124, name: "123"}], total: 2}},
+            status: 200,
+            statusText: 'OK',
+            headers: {},
+            config: {}
+          };
+        jest.spyOn(mockHttpService, 'get').mockImplementation(() => of(mock));
+        const result: Character[] = await marvelService.getAllCharacters();
+        expect(result).toHaveLength(2);
+        expect(result[0].id).toEqual(123);
+        expect(result[1].id).toEqual(124);
     });
 })
